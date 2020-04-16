3ab202a87743f7d56fa75b317de9555d3d607b8c
YunaLiou <57693154+YunaLiou@users.noreply.github.com>
2020-04-16 14:22:53 +0800

    Add files via upload

---
+++ unet.py
@@ -0,0 +1,166 @@
+import os
+# os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+import cv2
+import numpy as np
+from keras.models import *
+from keras.layers import Input, merge, Conv2D, MaxPooling2D, UpSampling2D, Dropout, Cropping2D, concatenate
+from keras.optimizers import *
+from keras.callbacks import ModelCheckpoint, LearningRateScheduler
+from keras import backend as keras
+from keras_preprocessing.image import array_to_img
+
+
+class Unet(object):
+    def __init__(self, img_rows=256, img_cols=512):
+        self.img_rows = img_rows
+        self.img_cols = img_cols
+
+    def load_data(self):
+        image_list = []
+        gt_list = []
+        test_list = []
+        all_list = [('./dataset/image/', image_list), ('./dataset/ground_truth/', gt_list),
+                    ('./dataset/test_image/', test_list)]
+
+        for filepath, filelist in all_list:
+            for filename in os.listdir(filepath):
+                filelist.append(os.path.join(filepath, filename))
+            filelist.sort()
+
+        folder_image_count = len(image_list)
+        folder_gt_count = len(gt_list)
+        folder_test_count = len(test_list)
+
+        imgs_train = np.zeros(shape=(folder_image_count, 256, 512, 1))
+        imgs_mask_train = np.zeros(shape=(folder_gt_count, 256, 512, 1))
+        imgs_test = np.zeros(shape=(folder_test_count, 256, 512, 1))
+
+        x = 0
+
+        all_matrix = [(image_list, imgs_train), (gt_list, imgs_mask_train),
+                      (test_list, imgs_test)]
+
+        for listname, matrixname in all_matrix:
+            for filepath in listname:
+                a = cv2.imread(filepath, 0)
+                a = a.reshape(256, 512, 1)
+                imgs_train[x] = a
+                x += 1
+            x = 0
+
+        imgs_train = imgs_train.astype('float32')
+        imgs_mask_train = imgs_mask_train.astype('float32')
+        imgs_test = imgs_test.astype('float32')
+
+        imgs_train /= 255
+        imgs_mask_train /= 255
+        imgs_test /= 255
+
+        imgs_mask_train[imgs_mask_train > 0.5] = 1.0
+        imgs_mask_train[imgs_mask_train <= 0.5] = 0.0
+
+        return imgs_train, imgs_mask_train, imgs_test
+
+    def get_unet(self):
+        inputs = Input((self.img_rows, self.img_cols, 1))
+        # the framework of unet network
+        conv1 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(inputs)
+        print("conv1 shape:", conv1.shape)
+        conv1 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv1)
+        print("conv1 shape:", conv1.shape)
+        pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+        print("pool1 shape:", pool1.shape)
+
+        conv2 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool1)
+        print("conv2 shape:", conv2.shape)
+        conv2 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv2)
+        print("conv2 shape:", conv2.shape)
+        pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+        print("pool2 shape:", pool2.shape)
+
+        conv3 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool2)
+        print("conv3 shape:", conv3.shape)
+        conv3 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv3)
+        print("conv3 shape:", conv3.shape)
+        pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+        print("pool3 shape:", pool3.shape)
+
+        conv4 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool3)
+        conv4 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv4)
+        drop4 = Dropout(0.5)(conv4)
+        pool4 = MaxPooling2D(pool_size=(2, 2))(drop4)
+
+        conv5 = Conv2D(1024, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool4)
+        conv5 = Conv2D(1024, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv5)
+        drop5 = Dropout(0.5)(conv5)
+
+        up6 = Conv2D(512, 2, activation='relu', padding='same', kernel_initializer='he_normal')(
+            UpSampling2D(size=(2, 2))(drop5))
+        merge6 = concatenate([drop4, up6], axis=3)
+        conv6 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge6)
+        conv6 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv6)
+
+        up7 = Conv2D(256, 2, activation='relu', padding='same', kernel_initializer='he_normal')(
+            UpSampling2D(size=(2, 2))(conv6))
+        merge7 = concatenate([conv3, up7], axis=3)
+        conv7 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge7)
+        conv7 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv7)
+
+        up8 = Conv2D(128, 2, activation='relu', padding='same', kernel_initializer='he_normal')(
+            UpSampling2D(size=(2, 2))(conv7))
+        merge8 = concatenate([conv2, up8], axis=3)
+        conv8 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge8)
+        conv8 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv8)
+
+        up9 = Conv2D(64, 2, activation='relu', padding='same', kernel_initializer='he_normal')(
+            UpSampling2D(size=(2, 2))(conv8))
+        merge9 = concatenate([conv1, up9], axis=3)
+        conv9 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge9)
+        conv9 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv9)
+        conv9 = Conv2D(2, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv9)
+        conv10 = Conv2D(1, 1, activation='sigmoid')(conv9)
+
+        model = Model(input=inputs, output=conv10)
+        model.compile(optimizer=Adam(lr=1e-4), loss='binary_crossentropy', metrics=['accuracy'])
+        return model
+
+    def train(self):
+        print("loading data")
+        imgs_train, imgs_mask_train, imgs_test = self.load_data()
+
+        print(imgs_train.shape)
+        print(imgs_mask_train.shape)
+        print(imgs_test.shape)
+
+        print("loading data done")
+        model = self.get_unet()
+        print("got unet")
+        model_checkpoint = ModelCheckpoint('my_unet.hdf5', monitor='loss', verbose=1, save_best_only=True)
+        print('Fitting model...')
+        model.fit(imgs_train, imgs_mask_train, batch_size=2, nb_epoch=10, verbose=1, validation_split=0.2, shuffle=True,
+                  callbacks=[model_checkpoint])
+
+        print('predict test data')
+        testImg_mask = model.predict(imgs_test, batch_size=1, verbose=1)
+        np.save('./testImg_mask.npy', testImg_mask)
+
+    def save_img(self):
+        print("array to image")
+        imgs = np.load('./testImg_mask.npy')
+        for i in range(imgs.shape[0]):
+            img = imgs[i]
+            img = array_to_img(img)
+            img.save("./resultimage/result_%d.jpg" % i)
+
+
+if __name__ == '__main__':
+    # Adding the following two lines make the program on the GUP without error.
+    physical_devices = tf.config.experimental.list_physical_devices('GPU')
+    tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
+    # you need to revise the values of img_rows and img_cols to the values matched with your image's size.
+    myunet = Unet(img_rows=256, img_cols=512)
+
+
+    myunet.train()
+    myunet.save_img()
